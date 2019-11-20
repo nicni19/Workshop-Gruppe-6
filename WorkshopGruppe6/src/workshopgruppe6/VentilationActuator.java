6524a82d12092f7e2dbe9ec3bd0d696cebc12e24
telmerie <tes_7583@hotmail.com>
2019-11-20 14:27:38 +0100

    create

---
+++ WorkshopGruppe6/src/workshopgruppe6/VentilationActuator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshopgruppe6;
+
+/**
+ *
+ * @author tes_7
+ */
+public class VentilationActuator extends Acturator {
+    
+}
