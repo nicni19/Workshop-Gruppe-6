a300fa671aa88a2ace7b99df7ab496308c355298
nicni19 <55740458+nicni19@users.noreply.github.com>
2019-11-20 13:53:15 +0100

    Thing
    Lavet Thing klasse

---
+++ WorkshopGruppe6/src/workshopgruppe6/Thing.java
@@ -0,0 +1,27 @@
+
+package workshopgruppe6;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nicos
+ */
+public abstract class Thing {
+    private String name;
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void listToString(ArrayList<Thing> list){
+        for(Thing t : list){
+            t.toString();
+            System.out.println("/n");
+        }
+    }
+}
